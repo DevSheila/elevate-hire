ee43eaed8e33a56411809e9ac38db8dab14f6c29
DevSheila <devsheilawambui@gmail.com>
2024-10-01 16:11:32 +0300

    initialize project with tailwind and shadcn config

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React,{ useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='bg-red-300'>Elevate Hire</h1>
+
+    </>
+  )
+}
+
+export default App
